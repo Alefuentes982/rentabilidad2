a46982bff99834761267c32fdd976d444de0ed6e
Ale Fuentes <jano231982@gmail.com>
2020-10-15 15:14:33 -0300

    final2.0

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,31 +1,29 @@
 
 
-preciov =ARGV[1].to_i
-usuarios = ARGV[2].to_i
+preciov =ARGV[0].to_i
+usuarios = ARGV[1].to_i
 premium = ARGV[2].to_i
 gratis = ARGV[3].to_i
 gastos = ARGV[4].to_i
 
-un = (preciov*usuarios)-gastos.to_f
+un = (preciov*usuarios)-gastos
+
 if un > 0 
-  impuesto =((un*35)/100).to_f
-  un = un - impuesto
+  impuesto =(un*35)/100.to_f
+  un = un - impuesto.to_f
 end
 
+up =(preciov*premium)-gastos
 
-up =((preciov*premium)-gastos).to_f
 if up > 0
-  impuestop = ((up *35)/100)*2.to_f
-  up = up - impuestop
+  impuestop = (up*35)/100.to_f
+  impuestop2 = impuestop * 2
+  up = up - impuestop2.to_f
 end
 
+ug =(preciov*gratis)-gastos
 
-ug =(preciov*gratis)-gastos.to_f
+utotal = un + up + ug
 
-
-utilidadestotal =un+up+ug
-
-
-
-puts "el total de utilidades es:#{utilidadestotal}"
+puts "el total de utilidades es:#{utotal}"
 
